7c3b3a1f531ff6c6fddf9d78782a0ccca31311f5
henryba16 <161905558+henryba16@users.noreply.github.com>
2024-04-27 21:58:07 +0700

    Update web.py

--- web.py
+++ web.py
@@ -2,18 +2,6 @@ import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
 
-# Set relative file path to JPEG favicon
-favicon_path = "shutterstock_535124956-scaled.jpg"
-
-# Inject custom HTML header to set website icon
-st.markdown(
-    f"""
-    <link rel="shortcut icon" href="file://{favicon_path}" type="image/jpeg">
-    <link rel="icon" href="file://{favicon_path}" type="image/jpeg">
-    """,
-    unsafe_allow_html=True
-)
-
 # Set page configuration
 st.set_page_config(page_title="About me", page_icon=":tada:", layout="wide")
 
